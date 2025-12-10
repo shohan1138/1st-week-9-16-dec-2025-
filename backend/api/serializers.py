a859799d29293f4abc1ba89e9febf8a18ad3322e
shohan1138 <shohanurrahman1138@gmail.com>
2025-12-11 00:46:26 +0600

    Initial commit

---
+++ backend/api/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'confirm_password']    
+        extra_kwargs = {'password': {'write_only': True}}   
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user =User.objects.create_user(**validated_data)
+        return user
